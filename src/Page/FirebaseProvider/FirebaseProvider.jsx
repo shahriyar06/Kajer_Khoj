3cb1d7261a69dc64d4cbdf62c48ae87a72b2512e
Shahriyar <chyshahriyar@gmail.com>
2024-05-11 12:28:19 +0600

    Register page completed

--- src/Page/FirebaseProvider/FirebaseProvider.jsx
+++ src/Page/FirebaseProvider/FirebaseProvider.jsx
@@ -1,5 +1,5 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from './../../Firebase/firebase.config';
 
 
@@ -10,15 +10,36 @@ const FirebaseProvider = ({children}) => {
     const [user, setuser] = useState(null)
     const [ loading, setloading ] = useState(true)
 
+    // Create New User
+    const signup = (email, password) =>{
+        setloading(true)
+       return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
     // SignIn User
     const signin = (email, password) =>{
         setloading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
+
+    // Obserber
+    useEffect(() =>{
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+           
+              setuser(user)
+              setloading(false)
+           
+          });
+          return () => unsubscribe();
+    },[user])
+
     const allvalue = {
         user,
-        loading
+        loading,
+        signup,
+        signin
     }
     return (
         <div>
