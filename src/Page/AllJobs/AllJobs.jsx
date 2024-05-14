04630ce6bb484a6dea21c3e2c3108c17e86bca00
Shahriyar <chyshahriyar@gmail.com>
2024-05-14 21:23:59 +0600

    All jobs searchbar completed completed

--- src/Page/AllJobs/AllJobs.jsx
+++ src/Page/AllJobs/AllJobs.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 
 const AllJobs = () => {
 
     const loadalljobs = useLoaderData();
+    const [searchtitle, setsearchtitle] = useState("");
+
+    // Filter jobs based on search query
+    const filteredJobs = loadalljobs.filter(job => {
+        return job.jobtitle.toLowerCase().includes(searchtitle.toLowerCase());
+    });
+
 
     return (
         <div>
+            <div className="text-center"><h1 className="text-5xl font-bold">Here All of Our Jobs</h1></div>
+            <div className='lg:w-6/12 w-11/12 mx-auto my-6 relative'>
+                <input type="text" placeholder="Enter your email address" className="rounded-full h-14 pl-6 text-lg w-full text-[#080808] border border-[#947351]" value={searchtitle} onChange={(e) => setsearchtitle(e.target.value)} />
+
+                <button className="btn rounded-r-full h-14 md:w-36 w-24 btn-outline text-[#947351] hover:text-[#FFFFFF] hover:bg-[#947351] hover:border-[#947351] text-xl absolute right-0">Signup</button>
+            </div>
             <div className="container p-2 mx-auto sm:p-4 text-gray-100 dark:text-gray-800">
                 <div className="overflow-x-auto text-center">
                     <table className="min-w-full">
@@ -32,9 +46,9 @@ const AllJobs = () => {
                         </thead>
                         <tbody>
                             {
-                                loadalljobs.map((alljobs, idx) => <tr key={alljobs._id} className="border-b border-opacity-20 border-gray-700 dark:border-gray-300 bg-gray-900 dark:bg-gray-50">
+                                filteredJobs.map((alljobs, idx) => <tr key={alljobs._id} className="border-b border-opacity-20 border-gray-700 dark:border-gray-300 bg-gray-900 dark:bg-gray-50">
                                     <td className="p-3">
-                                        <p>{idx+1}</p>
+                                        <p>{idx + 1}</p>
                                     </td>
                                     <td className="p-3 w-72">
                                         <p>{alljobs.jobtitle}</p>
@@ -43,10 +57,10 @@ const AllJobs = () => {
                                         <p>{alljobs.name}</p>
                                     </td>
                                     <td className="p-3">
-                                        <p>{alljobs.jobpostdate.slice(0,10)}</p>
+                                        <p>{alljobs.jobpostdate.slice(0, 10)}</p>
                                     </td>
                                     <td className="p-3">
-                                        <p>{alljobs.applicationdeadline.slice(0,10)}</p>
+                                        <p>{alljobs.applicationdeadline.slice(0, 10)}</p>
                                     </td>
                                     <td className="p-3">
                                         <p>$ {alljobs.salaryrange}</p>
